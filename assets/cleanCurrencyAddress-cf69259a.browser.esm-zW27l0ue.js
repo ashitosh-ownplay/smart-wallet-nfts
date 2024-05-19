c1214eb9494d125a9469806f9dcfd873c50d57ac
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-05-19 14:55:36 +0000

    deploy: a9d42804601e5267db0aeaab9ffbd75c6af53b9e

--- assets/cleanCurrencyAddress-cf69259a.browser.esm-zW27l0ue.js
+++
@@ -1,1 +0,0 @@
-import{bS as e,bw as n}from"./index-UIkD6jaU.js";function a(r){return e(r)?n:r}export{a as c};
---
+++ assets/eth_estimateGas-C0gl_fAV.js
@@ -0,0 +1,1 @@
+import{x as s}from"./index-CmAFPhII.js";async function m(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{m as eth_estimateGas};
