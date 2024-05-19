b3c7fdf7bd4dd8bea7c5561b8469b6735dee6f87
ashitosh <ashitosh.bhade@ownplay.co>
2024-05-19 09:52:28 +0530

    Updates

--- assets/cleanCurrencyAddress-cf69259a.browser.esm-C2Aj10t2.js
+++
@@ -1,1 +0,0 @@
-import{bS as e,bw as n}from"./index-B0fTNjav.js";function a(r){return e(r)?n:r}export{a as c};
---
+++ assets/hasERC20Allowance-2c31c3b2.browser.esm-CBc7s69z.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/index-C820askT.js","assets/index-D_xrjtvU.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as l,aJ as w}from"./index-C820askT.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-C820askT.js").then(d=>d.h5),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),i=e.address;return(await s.read("allowance",[o,i])).gte(t)}export{g as h};
