5813e98e199559fc321eb34b3d71b73d0788a3fd
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-05-19 07:10:49 +0000

    deploy: 7dcf23ae1ab9856d2f83c4e6841ec4fddff78894

--- assets/hashMessage-CXzU5OWT.js
+++
@@ -1,2 +0,0 @@
-import{dV as n,d as a,k as i,n as o}from"./index-DEMNTMty.js";const c=`Ethereum Signed Message:
-`;function p(t,e){const r=typeof t=="string"?n(t):t.raw instanceof Uint8Array?t.raw:a(t.raw),s=n(`${c}${r.length}`);return i(o([s,r]),e)}export{p as h,c as p};
