1f675ed1047975fd019407c09fd8ef7670950848
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-05-22 18:24:37 +0000

    deploy: 41ebec4244fae1abb0c60d0f1927e7d6e04aab93

---
+++ assets/checkContractWalletSignature-BtHuwULC.js
@@ -0,0 +1,1 @@
+import{i as a}from"./isValidSignature-DbW0UVvM.js";import{q as t}from"./index-CmBUR8Sr.js";import{h as e}from"./hashMessage-CkQ8zBiP.js";const s="0x1626ba7e";async function g(r){if(!t(r.signature))throw new Error("The signature must be a valid hex string.");return await a({contract:r.contract,hash:e(r.message),signature:r.signature})===s}export{g as checkContractWalletSignature};
--- assets/eth_getTransactionCount-ByaYWNKD.js
+++
@@ -1,1 +0,0 @@
-import{o as n,dS as r}from"./index-DcGpi8Zt.js";async function u(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return r(e)}export{u as eth_getTransactionCount};
