60347b57ee5a6b9b8201ded317b3bd0c4af56e61
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-05-23 13:56:13 +0000

    deploy: 426a3143c5e094b642e17f12427beab730e819c1

--- assets/checkContractWalletSignature-C9zbKm0W.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./isValidSignature-DQp_DbA5.js";import{q as t}from"./index-KWJPhIT3.js";import{h as e}from"./hashMessage-BtM5b5ha.js";const s="0x1626ba7e";async function g(r){if(!t(r.signature))throw new Error("The signature must be a valid hex string.");return await a({contract:r.contract,hash:e(r.message),signature:r.signature})===s}export{g as checkContractWalletSignature};
