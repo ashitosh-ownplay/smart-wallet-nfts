400e145e72d321ddb5a523470857c67bec4e3ca4
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-06-17 16:26:15 +0000

    deploy: ab8289876376f676a9480768ee86b540a2f9a15b

--- assets/hasERC20Allowance-2c31c3b2.browser.esm-B3ed215V.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/index-BbpUP2yR.js","assets/index-B_SY1GJM.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as l,aS as w}from"./index-BbpUP2yR.js";async function g(e,a,t){const n=e.getProvider(),r=(await l(()=>import("./index-BbpUP2yR.js").then(d=>d.eN),__vite__mapDeps([0,1]))).default,s=new w(n,a,r,{},e.storage),o=await e.getSignerAddress(),i=e.address;return(await s.read("allowance",[o,i])).gte(t)}export{g as h};
