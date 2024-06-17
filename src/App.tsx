1859f2e0a5bd1a43ded8cfc34a749e8a0b44e9b2
ashitosh <ashitosh.bhade@ownplay.co>
2024-06-17 21:32:18 +0530

    add fix for img for local

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,26 @@ import HomePage from "./pages/home";
 import { useSnackbarContext } from "./contexts/snackbarContext";
 import "./App.css";
 import { Footer } from "./components/Footer";
+import { base, sepolia } from "thirdweb/chains";
+import { useState } from "react";
 
 function App() {
   const { toastState, handleCloseToast } = useSnackbarContext();
+  const [logoSrc, setLogoSrc] = useState<string>("");
+  const [isImgError, setImgError] = useState<boolean>(false);
 
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
     },
   });
+
+  const handleError = () => {
+    if (chainId == sepolia.name?.toLowerCase()) {
+      setLogoSrc("/logo.png");
+      setImgError(true);
+    }
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -33,7 +44,16 @@ function App() {
           paddingBottom="5rem"
           gap={4}
         >
-          <img src="/smart-wallet-nfts/logo.png"></img>
+          <img
+            src={`${
+              isImgError
+                ? logoSrc
+                : chainId === base.name?.toLowerCase()
+                ? "/smart-wallet-nfts/logo.png"
+                : "/dev-smart-wallet-nfts/logo.png"
+            }`}
+            onError={handleError}
+          ></img>
           <HomePage />
         </Box>
         <Footer />
