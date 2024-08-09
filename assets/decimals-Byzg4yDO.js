90cc43df2d76773ff4211fdd7cc846b278bf12f5
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-08-09 14:16:27 +0000

    deploy: 4891ac3a496ad05aa539fbd436173d13a06fee16

---
+++ assets/decimals-Byzg4yDO.js
@@ -0,0 +1,1 @@
+import{av as a,cc as e}from"./index-C-LyHPfb.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
