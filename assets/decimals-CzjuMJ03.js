2ed5d93cdc87c18bac31da7557bc6083c732f028
ashitosh-ownplay <ashitosh-ownplay@users.noreply.github.com>
2024-09-24 08:04:52 +0000

    deploy: ef5f48187c1bd8ba76f691a50d2e5548e9dafc68

---
+++ assets/decimals-CzjuMJ03.js
@@ -0,0 +1,1 @@
+import{av as a,cc as e}from"./index-DXAu4oGA.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
